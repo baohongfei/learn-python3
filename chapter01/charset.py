7dd28b7f1971f3e2f299b0a8ad1ed0b604ea389b
baohongfei <github@baohongfei.com>
2016-09-28 16:45:35 +0800

    add

---
+++ chapter01/charset.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+print("包含中文的str")
+
+print(ord('A'))
+
+print(ord('中'))
+
+print(chr(66))
+
+print(chr(25991))
+
+print('\u4e2d\u6587')
+
+x =  b'ABC'
+print(x)
+
+# TODO
---
+++ chapter01/listTuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+classmates = ['Michael','Bob','Tracy']
+print (classmates)
+print(len(classmates))
+print(classmates[-2])
+
+classmates.append("Adam")
+print(classmates)
+
+classmates.insert(1,"Jack")
+print(classmates)
+
+classmates.pop()
+print(classmates)
+
+# tuple
