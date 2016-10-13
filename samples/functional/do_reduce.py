bef97831ba29e41d76f236608d6ab01f60fc3d4f
baohongfei <github@baohongfei.com>
2016-10-13 17:54:04 +0800

    u

---
+++ samples/functional/do_reduce.py
@@ -0,0 +1,17 @@
+#!/urs/bin/env python3
+# -*- coding: utf-8 -*-
+
+from functools import reduce
+
+CHAR_TO_INT = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9
+}
